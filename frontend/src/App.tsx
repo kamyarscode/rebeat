09630d8fa0a4be568f2141ec8120dd4daed9cda4
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-02-08 14:19:06 -0700

    better stagger

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,17 @@ function App() {
     <main className="flex flex-col gap-4 container mx-auto justify-center items-center h-full px-8">
       <div className="flex flex-col gap-6 max-w-sm">
         <div className="space-y-1">
-          <h1 className="text-2xl font-bold animate-in fade-in-0 slide-in-from-bottom duration-1000">
+          <h1 className="text-2xl font-bold animate-in fade-in-0 slide-in-from-bottom duration-300">
             Welcome to Rebeat
           </h1>
-          <p className="text-sm text-muted-foreground animate-in fade-in-0 slide-in-from-bottom delay-100 duration-1000">
+          <p className="text-sm text-muted-foreground animate-in fade-in-0 slide-in-from-bottom duration-500">
             Rebeat will add a playlist of the music you listened to on your runs
             to your strava activities.
           </p>
         </div>
         <div className="flex flex-col gap-2">
-          <ConnectStrava className="animate-in fade-in slide-in-from-bottom delay-200 duration-1000" />
-          <ConnectSpotify className="animate-in fade-in-0 slide-in-from-bottom delay-300 duration-1000" />
+          <ConnectStrava className="animate-in fade-in slide-in-from-bottom duration-700" />
+          <ConnectSpotify className="animate-in fade-in-0 slide-in-from-bottom duration-700" />
         </div>
       </div>
     </main>
