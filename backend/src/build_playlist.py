609d960637bb0e3366c5a3e0438eaa6af5a4aae1
kamyar <52869955+kamyarscode@users.noreply.github.com>
2025-02-10 16:43:09 -0700

    add songs to playlist functionality

--- backend/src/build_playlist.py
+++ backend/src/build_playlist.py
@@ -24,33 +24,48 @@ def create_playlist(user_id: str, playlist_name: str, playlist_description: str,
         public: bool - True if playlist is public, False if private.
 
     Returns:
-        dict: A dictionary containing the playlist details.
+        str: ID of the playlist.
 
     """
 
     SPOTIFY_CREATE_PLAYLIST_URL = "https://api.spotify.com/v1/users/{user_id}/playlists"
-    params = {
+    data = {
     "name": playlist_name,
     "description": playlist_description,
     "public": public
     }
 
     try:
-        response = requests.post(SPOTIFY_CREATE_PLAYLIST_URL.format(user_id=user_id), headers=with_auth_headers(), json=params)
+        response = requests.post(SPOTIFY_CREATE_PLAYLIST_URL.format(user_id=user_id), headers=with_auth_headers(), json=data)
         response.raise_for_status()
-        # Return ID of playlist
+
+        # Return ID of playlist for now.
         return response.json()['id']
     
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}
 
 
-def add_songs(recently_played_songs_array: list, playlist_id: str) -> dict:
+def add_songs(recently_played_songs_id_array: list, playlist_id: str) -> dict:
     """
     Reference:
     GET https://api.spotify.com/v1/playlists/{playlist_id}
     POST https://api.spotify.com/v1/playlists/{playlist_id}/tracks
     """
+    SPOTIFY_ADD_TO_PLAYLIST_URL = "https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
 
+    data = {
+    "uris": [f"spotify:track:{x}" for x in recently_played_songs_id_array],
+    "position": 0
+    }
 
-    pass
+    try:
+        response = requests.post(SPOTIFY_ADD_TO_PLAYLIST_URL.format(playlist_id=playlist_id), headers=with_auth_headers(), json=data)
+        response.raise_for_status()
+
+        # Return response object
+        return response.json()
+    
+    except requests.exceptions.RequestException as e:
+        return {"error": str(e)}
+    
