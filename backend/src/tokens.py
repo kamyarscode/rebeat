98d35eb083101360b8030834691ffd46bb680d78
kamyar <52869955+kamyarscode@users.noreply.github.com>
2025-02-10 16:47:15 -0700

    add token refresh function starter code

---
+++ backend/src/tokens.py
@@ -0,0 +1,8 @@
+def refresh_tokens(access_token, refresh_token):
+    # Refresh the tokens starter code
+    tokens = {
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    }
+
+    return tokens
