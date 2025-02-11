4f4337c4d928a33e2269823c194522118a0f3ea2
kamyar <52869955+kamyarscode@users.noreply.github.com>
2025-02-11 15:48:17 -0700

    apparently i cant merge. fixed merge conflict properly

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
 from fastapi import FastAPI, Request, Query
+from fastapi.responses import RedirectResponse
 from src.strava_interface import subscribe_to_strava
 import uvicorn
 from helpers import generate_random_string
@@ -50,8 +49,52 @@ def exchange_code_for_access_token(code: str) -> str:
 # More info at: https://developer.spotify.com/documentation/web-api/tutorials/code-flow
 @app.get("/login")
 def login():
+    url = "https://accounts.spotify.com/authorize"
 
-    return {"message": "Login reached"}
+    # TODO: Store state and compare to the state in the callback.
+    state = generate_random_string(16)
+    scope = "user-read-private user-read-email playlist-modify-private user-read-recently-played"
+    params = {
+        "response_type": "code",
+        "client_id": client_id,
+        "scope": scope,
+        "redirect_uri": redirect_uri,
+        "state": state,
+    }
+
+    return RedirectResponse(f"{url}?{urlencode(params)}")
+
+# Receives the code and state from spotify, and then exchanges the code for an access token
+@app.get("/callback")
+async def callback(request: Request):
+    # TODO: Store state and compare to the state here.
+    code: str | None = request.query_params.get("code")
+    state: str | None = request.query_params.get("state")
+    error: str | None = request.query_params.get("error")
+
+    # If there is an error, redirect to the frontend with the error.
+    if error:
+        return RedirectResponse(url=os.getenv("FRONTEND_URL") + "/error?error=" + error)
+
+    # If there is no state, redirect to the frontend with an error.
+    if not state:
+        return RedirectResponse(url=os.getenv("FRONTEND_URL") + "/error?error=no_state")
+
+    # If there is no code, redirect to the frontend with an error.
+    if not code:
+        return RedirectResponse(url=os.getenv("FRONTEND_URL") + "/error?error=no_code")
+
+    token_response = exchange_code_for_access_token(code)
+    print(token_response)
+
+    # TODO: Store the access token in the database associated with a user.
+    # For now, just redirect to the frontend with the access token. (bad)
+    return RedirectResponse(
+        url=os.getenv("FRONTEND_URL")
+        + "?access_token="
+        + token_response["access_token"]
+    )
+
 
 # Strava GET request to verify the callback URL.
 @app.get("/strava/webhook")
---
+++ backend/src/strava_interface.py
@@ -0,0 +1,31 @@
+import requests
+
+def subscribe_to_strava(CLIENT_ID, CLIENT_SECRET, CALLBACK_URL, VERIFICATION_TOKEN):
+    """
+    Function to create a subscription with Strava.
+
+    Args:
+        CLIENT_ID: str - Strava client ID.
+        CLIENT_SECRET: str - Strava client secret.
+        CALLBACK_URL: str - URL where Strava will send updates.
+        VERIFICATION_TOKEN: str - Token to verify the subscription.
+
+    Returns:
+        None - prints success message if subscription is successful.
+    """
+    STRAVA_SUBSCRIPTION_URL = "https://www.strava.com/api/v3/push_subscriptions"
+
+    data = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "callback_url": CALLBACK_URL,
+        "verify_token": VERIFICATION_TOKEN
+    }
+    response = requests.post(STRAVA_SUBSCRIPTION_URL, json=data)
+    
+    if response.status_code == 201:
+        print("Successfully subscribed to Strava updates")
+    else:
+        print("Failed to subscribe:", response.json())
+
+
