dfbdc58b09af7e9325427d7421fce92f529b3e8d
kamyar <52869955+kamyarscode@users.noreply.github.com>
2025-06-01 16:35:35 -0600

    add functionality to get songs played before timestamp of end time down to start time

--- backend/src/get_songs.py
+++ backend/src/get_songs.py
@@ -1,34 +1,37 @@
 import requests
 from helpers import with_auth_headers
+import json
+import pprint
+from datetime import date, datetime
+from time_utils import iso_to_unix
+from  typing import List
 
+"""
+Get recently played songs.
+GET https://api.spotify.com/v1/me/player/recently-played
 
-def get_recently_played_using_time(after):
+Parameters/Args:
+    limit: int - The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
 
-    # Get time from source ie Strava. Leave now for testing.
-    # after = datetime_to_iso_test()
-    """
-    Get recently played songs.
-    GET https://api.spotify.com/v1/me/player/recently-played
+    after: int - A Unix timestamp in milliseconds. Returns all items after (but not including) this cursor position.
+                If after is specified, before must not be specified.
+                Example: after=1484811043508
 
-    Parameters/Args:
-        limit: int - The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+    before: int - A Unix timestamp in milliseconds. Returns all items before (but not including) this cursor position.
+                If before is specified, after must not be specified.
 
-        after: int - A Unix timestamp in milliseconds. Returns all items after (but not including) this cursor position.
-                    If after is specified, before must not be specified.
-                    Example: after=1484811043508
+Returns:
+    list: A list of song names.
 
-        before: int - A Unix timestamp in milliseconds. Returns all items before (but not including) this cursor position.
-                    If before is specified, after must not be specified.
+"""
+def get_recently_played_using_time(before: str, start_time: str) -> List[str]:
 
-    Returns:
-        list: A list of song names.
-
-    """
     SPOTIFY_RECENTLY_PLAYED_URL = "https://api.spotify.com/v1/me/player/recently-played"
-
-    # TODO: Add docs here to explain "after" later
+    recently_played_song_names = []
+    recently_played_song_id = []
+    # TODO: Add docs here to explain "before" later
     params = {
-        "after": after,
+        "before": before
     }
 
     # Make API request to Spotify here.
@@ -41,14 +44,26 @@ def get_recently_played_using_time(after):
 
     # Get response object.
     response_json = response.json()
+  
+    for item in response_json["items"]:
+        # convert both times to format for comparison. 
+        played_at_unix_milliseconds = iso_to_unix(item["played_at"])
+        start_time_unix_milliseconds = iso_to_unix(start_time)
 
-    # Map function to get track names from response.
-    recently_played_song_names = list(
-        map(lambda x: x["track"]["name"], response_json["items"])
-    )
-    recently_played_song_id = list(
-        map(lambda x: x["track"]["id"], response_json["items"])
-    )
+        # logic to check if song played is after start time.
+        if played_at_unix_milliseconds > start_time_unix_milliseconds:
+            recently_played_song_id.append(item["track"]["id"])
+            recently_played_song_names.append(item["track"]["name"])
+
 
     # Return list of song names for testing, return id for actual use.
-    return recently_played_song_names, recently_played_song_id
+    return recently_played_song_id
+
+### Rough logic:
+
+"""
+Get all songs before the end of activity. i.e finish activity at 5:30, get all songs before 5:30.
+Check played_at time for each song. 
+If played_at is greater than the after parameter, don't add to playlist. 
+
+"""
---
+++ backend/src/time_utils.py
@@ -0,0 +1,65 @@
+import time
+from dateutil import parser
+import math
+import datetime
+
+
+"""Converts an ISO 8601 formatted string to a Unix timestamp (milliseconds since epoch).
+
+Args:
+    iso_string: The ISO 8601 formatted string.
+
+Returns:
+    The Unix timestamp as a float.
+"""
+def iso_to_unix(iso_string):
+
+    dt_object = parser.parse(iso_string)
+    timestamp = time.mktime(dt_object.timetuple())
+    return timestamp * 1000  # Convert to milliseconds
+
+
+# Time comes in from Strava.
+def get_time():
+    pass
+
+
+# comes in in minutes
+# convert to A Unix timestamp in milliseconds for spotify.
+def normalize_time(start, end):
+
+    normalized = map(lambda x: x * 1000 * 60, [start, end])
+    return list(normalized)
+
+
+# Set up test for passing input from Strava for now.
+def datetime_to_iso_test():
+    # Get the current time as datetime object to conver later.
+    end_time = datetime.datetime.utcnow()
+
+    # Calculate time 20 minutes ago, returns datetime object.
+    start_time = end_time - datetime.timedelta(minutes=15)
+
+    # Convert 5 minutes ago to ISO 8601 format (Strava format).
+    start_iso_string = start_time.isoformat()
+    end_iso_string = end_time.isoformat()
+
+    # Convert to Unix timestamp in milliseconds (What Spotify wants).
+    start_unix_milliseconds = iso_to_unix(start_iso_string) * 1000
+    end_unix_milliseconds = iso_to_unix(end_iso_string) * 1000
+
+    # Truncate and get rid of decimal points.
+    start_time = math.trunc(start_unix_milliseconds)
+    end_time = math.trunc(end_unix_milliseconds)
+
+    # remove decimal from iso start time iso string.
+    truncated_start_time_iso = start_iso_string.split(".")[0] + "Z"
+
+    # Return truncated epoch time in milliseconds.
+    return truncated_start_time_iso, end_time
+
+
+
+# Compare two dates in ISO 8601 format and return True if the first date is before the second date.
+def compare_dates():
+    pass
